fe87e725db36d8035d766ab66a7c2cfa1ad2f115
nur zazin <nurza.cool@gmail.com>
2021-08-12 10:35:56 +0700

    ad hello

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -2,26 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/zazin/test-plugin/gateway"
 	"net/http"
 )
 
-func init() {
-	fmt.Println("new plugin loaded!!!")
+type greeting string
+
+func (g greeting) Greet(ctx context.Context) (http.Handler, error) {
+	return gateway.New(ctx)
 }
 
-var ClientRegisterer = registerer("new-plugin")
-
-type registerer string
-
-func (r registerer) Run(f func(
-	name string,
-	handler func(context.Context, map[string]interface{}) (http.Handler, error),
-)) {
-	f(string(r), func(ctx context.Context, extra map[string]interface{}) (http.Handler, error) {
-		return gateway.New(ctx)
-	})
-}
-
-func main() {}
+// exported
+var Greeter greeting
