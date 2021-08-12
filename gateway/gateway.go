063fac648e4e84b40d799373328a3911df8616f9
nur zazin <nurza.cool@gmail.com>
2021-08-12 08:17:30 +0700

    ad hello

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -3,31 +3,31 @@ package gateway
 import (
 	"context"
 	"fmt"
+	helloworldpb "github.com/zazin/test-plugin/proto"
 	"google.golang.org/grpc"
 	"io"
 	"log"
 	"net/http"
 	"time"
-	helloworldpb "github.com/zazin/test-plugin/proto"
 )
 
 const portServerHello = ":50051"
 
 func HelloPage() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("zazin start grpc");
+		fmt.Println("zazin start grpc")
 		conn, err := grpc.Dial(portServerHello, grpc.WithInsecure())
 		if err != nil {
 			log.Fatalf("Could not connect to service: %v", err)
 		}
-		fmt.Println("zazin connected grpc");
+		fmt.Println("zazin connected grpc")
 		defer func(conn *grpc.ClientConn) {
 			err := conn.Close()
 			if err != nil {
 				log.Fatalf("Could not close connection: %v", err)
 			}
 		}(conn)
-		fmt.Println("zazin after connecting grpc");
+		fmt.Println("zazin after connecting grpc")
 		helloServiceClient := helloworldpb.NewGreeterClient(conn)
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
